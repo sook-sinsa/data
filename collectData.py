a0de9ca08eb5edb15dd56f596b89590049ee6f9f
DESKTOP-TAURE2M\stell <stellar1198@gmail.com>
2020-09-10 13:10:02 +0900

    add modify price information - from saleprice to originprice

--- collectData.py
+++ collectData.py
@@ -11,7 +11,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 ###################################################################################
 
 start = time.time()
-imgNum = 0  # 0-449, 각 페이지에서 총 450개 수집
+productNum = 0  # 0-449, 각 페이지에서 총 450개 수집
 urlCode = [[1001, 1010, 1011, 1002, 1003, 1005, 1004, 1006, 1008],
             [2022, 2001, 2002, 2017, 2003, 2020, 2019, 2006, 2018, 2004, 2008, 2007, 2009, 2013, 2012, 2016, 2021, 2014, 2015],
             [20006, 20007, 20008, 20002],
@@ -52,7 +52,7 @@ try:
 except:
     wb = openpyxl.Workbook()
     sheet = wb.active
-    sheet.append(["대 카테고리", "세부 카테고리", "브랜드 명", "제품 명", "가격", "상세 주소"])
+    sheet.append(["대 카테고리", "세부 카테고리", "제품 번호", "브랜드 명", "제품 명", "가격", "상세 주소"])
     print("새로 파일을 만들었습니다")
 
 # options = webdriver.ChromeOptions()
@@ -74,23 +74,21 @@ for page in range(1, 6) :
         brand = c.find_element_by_css_selector("div.article_info > p.item_title > a").text.strip()
         product = c.find_element_by_css_selector("p.list_info > a").text.strip()
         price = c.find_element_by_css_selector("p.price").text.strip().replace(",", "").replace("원", "")
-        priceIdx = price.find(" ")
-        price = int(price[priceIdx+1:])
+        if ' ' in price:
+            priceIdx = price.find(" ")
+            price = int(price[priceIdx+1:])
+        else:
+            price = int(price)
         detailUrl = c.find_element_by_css_selector("div.list_img > a").get_attribute("href")
 
         imgSrc = c.find_element_by_css_selector("div.list_img > a > img").get_attribute("src")
-        urlretrieve(imgSrc, 'collected/image/' + str(bigCategory) + '/' + str(smallCategory) + '/' + str(imgNum) + '.png')
+        urlretrieve(imgSrc, 'collected/image/' + str(bigCategory) + '/' + str(smallCategory) + '/' + str(productNum) + '.png')
 
         print(priceIdx)
         print(brand, product, price, detailUrl, sep=" / ")
 
-        sheet.append([bigCategory, smallCategory, brand, product, price, detailUrl])
-        imgNum += 1
-
-        if imgNum == 5 :
-            break
-    if page == 1 :
-        break
+        sheet.append([bigCategory, smallCategory, productNum, brand, product, price, detailUrl])
+        productNum += 1
 
 ###################################################################################
 
